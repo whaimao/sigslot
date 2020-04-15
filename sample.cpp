d72bd02be0053093dcbd1a260e3b8433a56bf507
wanghaimao <whaimao@foxmail.com>
2020-04-15 16:49:15 +0800

    增加 Sender 和 Recevier 类; note: 需求：需要Sender类信息传到Receiver类 前期工作： 1. 在发送类定义 sigslot::signal2<std::string, int> signalSender; //2表示需要传输两个参数，参数类型根据需求定 2. 需要在发送类中调用 signalSender("test", 1); //具体在什么地方根据需求调用 3. 接受类中定义一个和 signalSender 参数类型一致的函数 void onRecevier(std::string message, int type){ }, 当发送类调用signalSender("test", 1)时，就把信息从Sender类传到Receiver类 绑定： Sender sender; Receiver rec; sender.signalSender.connect(&rec, &Receiver::onRecevier); //绑定 sender.help();// signalSender的调用

---
+++ sample.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string.h>
+#include "sigslot.h"
+using namespace sigslot;
+class Sender{
+	public:
+		sigslot::signal2<std::string, int> signalSender;
+		void help(){
+			signalSender("help", 1);
+		}
+		void help2(){
+			signalSender("help,hahah,just test");
+		}
+	
+}
+class Recevier:public sigslot::has_slots<>{
+	public:
+		void onRecevier(std::string message, int type){//参数与Sender 中的signalSender要一致
+			if(type == 1){
+				std::cout << "I'am help() slot" <<std::endl;
+			}else{
+				std::cout << "I'm help2() slot" << std::end1;
+			}
+		}
+}
+int main(void){
+	
+	Sender s1;
+	Recevier r1;
+	s1.signalSender.connect(&r1, &Recevier::onRecevier);
+	sleep(3);
+	s1.help();
+	sleep(3);
+	s1.help2();
+	sleep(3);
+	s1.signalSender.disconnect(&r1);
+	return 0;
+}
